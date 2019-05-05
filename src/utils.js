ed4b8b883c4e0cb1ca41bb83eda722c777508103
Sabrina Tjeng and Nicole Budiman <sabriele+nicole.budiman@gmail.com>
2019-05-05 19:52:56 +0800

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,10 @@
+// returns the winning player or null for a tie but just for learning mocks,
+// let's pretend this makes a call to a third party API
 function getWinner(player1, player2) {
     const winningNumber = Math.random()
-    return winningNumber < 1 / 3
+    return winningNumber < 1 / 2
         ? player1
-        : winningNumber < 2 / 3
+        : winningNumber > 1 / 2
             ? player2
             : null
 }
